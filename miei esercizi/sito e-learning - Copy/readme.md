556d07b9e54009b8fe36c76181e4023624c5522b
Michele Spinelli <michele.spinelli@reti.it>
2019-11-14 16:47:53 +0100

    crea cartella miei esercizi

---
+++ miei esercizi/sito e-learning - Copy/readme.md
@@ -0,0 +1,8 @@
+
+Questa pagina è volta alla pratica della griglia CSS per la creazione
+di un piccolo sito di e-learning
+Di seguito altre funzionalità CSS di cui ho potuto fare pratica
+con questo esercizio:
+- flexbox
+- object-fit
+- git branching
---
+++ miei esercizi/studio del canvas/1/index.js
@@ -0,0 +1,50 @@
+
+function draw() {
+    var canvas = document.getElementById('tutorial');
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+
+        // draw several squares
+        // ctx.fillRect(25, 25, 100, 100);
+        // ctx.clearRect(45, 45, 60, 60);
+        // ctx.strokeRect(50, 50, 50, 50);
+
+        // draw a triangle
+        // ctx.beginPath();
+        // ctx.moveTo(75, 50);
+        // ctx.lineTo(100, 75);
+        // ctx.lineTo(100, 25);
+        // ctx.fill();
+
+        // draw a smiley face
+        // ctx.beginPath();
+        // ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+        // ctx.moveTo(110, 75);
+        // ctx.arc(75, 75, 35, 0, Math.PI, false);  // Mouth (clockwise)
+        // ctx.moveTo(65, 65);
+        // ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
+        // ctx.moveTo(95, 65);
+        // ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
+        // ctx.stroke();
+
+        // filled triangle
+        ctx.beginPath();
+        ctx.moveTo(25, 25);
+        ctx.lineTo(105, 25);
+        ctx.lineTo(25, 105);
+        ctx.fill();
+
+        // stroked triangle
+        ctx.beginPath();
+        ctx.moveTo(125, 125);
+        ctx.lineTo(125, 45);
+        ctx.lineTo(45, 125);
+        ctx.closePath();
+        ctx.stroke();        
+
+    }
+}
+
+window.addEventListener("load", function () {
+    draw();
+});
