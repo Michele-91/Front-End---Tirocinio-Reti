f8ff4b779a7f455fb77e16f8f95b371e35490d8d
Michele Spinelli <michele.spinelli@reti.it>
2019-11-18 16:07:17 +0100

    esercizio 7 sulle funzioni

---
+++ 5.7 funzioni/7.js
@@ -0,0 +1,22 @@
+// Write a JavaScript function that accepts a string as a parameter and counts the number of vowels within the string
+
+function vowelNum(str) {
+    let vowelCount = 0;
+    for(let i = 0; i < str.length; i++) {
+        switch(str[i]) {
+            case 'a': case 'A':
+            case 'e': case 'E':
+            case 'i': case 'i':
+            case 'o': case 'o':
+            case 'u': case 'U':
+            vowelCount = vowelCount + 1;
+            break;
+            // default:
+            // console.log('se vedi questo messaggio si è verificato un errore inaspettato');   
+            // break;
+        }
+    }
+    return vowelCount;
+}
+
+console.log(vowelNum('supercalifragilistichespiralidoso'));
